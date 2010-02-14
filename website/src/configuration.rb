971202c698b2afa565528b647d3e3c811d828fc9
Joe Cheng <joe@joecheng.com>
2010-02-13 23:22:43 -0800

    v2 of 2010 site. Now using StaticMatic

---
+++ website/src/configuration.rb
@@ -0,0 +1,2 @@
+PlayerRegistrationUrl = 'http://public.serviceu.com/registration/default.asp?OrgID=11910&EventID=4513567&OccID=160190237'
+BanquetRegistrationUrl = 'http://public.serviceu.com/registration/default.asp?OrgID=11910&EventID=4521802&OccID=160573484'
