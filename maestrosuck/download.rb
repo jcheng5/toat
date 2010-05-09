b994bca33b9a784889684ead6d40221d08025e59
Joe Cheng <joe@joecheng.com>
2010-05-09 01:34:29 -0700

    maestrosuck outputs as XML, downloads images

--- maestrosuck/download.rb
+++ maestrosuck/download.rb
@@ -5,9 +5,11 @@
 
 require 'rubygems'
 require 'hpricot'
+I_KNOW_I_AM_USING_AN_OLD_AND_BUGGY_VERSION_OF_LIBXML2 = true
 require 'mechanize'
 require 'pp'
 require 'highline/import'
+require 'builder'
 #require 'uri'
 #require 'net/http'
 #require 'net/https'
@@ -17,6 +19,16 @@ $agent = Mechanize.new
 class NilClass
   def content()
     nil
+  end
+end
+
+class Hash2Object
+  def initialize(hash)
+    @hash = hash
+  end
+  def method_missing(symbol, *args)
+    return super(symbol, args) if args.length > 0
+    @hash[symbol]
   end
 end
 
@@ -31,13 +43,23 @@ end
 def get_item(id)
   item = $agent.get("https://secure.maestroweb.com/Details.aspx?OrgID=929&ItemID=#{id}&selection=36")
   return nil unless item.search('#lblItemName').first
+  
+  img_url = nil
+  img_remote_url = 'https://secure.maestroweb.com/' + item.search('#imgItem').first['src']
+  img_file = 'images/' + File.basename(img_remote_url)
+  begin
+    $agent.get(img_remote_url).save(img_file)
+    img_url = 'file:///' + File.join(Dir.pwd, img_file)
+  rescue
+  end
+  
   data = {
     :id => id,
-    :name => item.search('#lblItemName').first.content,
-    :desc => item.search('#lblItemDesc').first.content,
-    :value => item.search('#lblItemValue').first.content,
-    :donor => item.search('#lblItemDonors').first.content,
-    :img => 'https://secure.maestroweb.com/' + item.search('#imgItem').first['src']
+    :name => item.search('#lblItemName').first.content.strip,
+    :desc => item.search('#lblItemDesc').first.content.strip,
+    :value => item.search('#lblItemValue').first.content.strip,
+    :donor => item.search('#lblItemDonors').first.content.strip,
+    :img => img_url
   }
 end
 
@@ -58,6 +80,27 @@ end
 password = ask("Password: ") {|q| q.echo='*'}
 login('Cheng', password)
 
-100.times do |i|
-  item = get_item(i)
+xml = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>0)
+xml.Root do |root|
+  150.times do |i|
+    item = get_item(i)
+    $stderr.print(item ? '1' : '0')
+    next unless item
+    root.item do |b|
+      b.image(:href => item[:img])
+      puts
+      b.name(item[:name])
+      puts
+      b.desc(item[:desc])
+      puts
+      b.value_label("Value: ")
+      b.value(item[:value])
+      puts
+      b.donor_label("Donated by: ")
+      b.donor(item[:donor])
+      puts
+      b.id("Auction lot #{item[:id]}")
+      puts
+    end
+  end
 end
